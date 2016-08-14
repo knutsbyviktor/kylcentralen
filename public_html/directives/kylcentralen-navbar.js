41f74318783d9d27b48dff285917ce3cef6ca2e1
Viktor Knutsby <knutsbyviktor@gmail.com>
2016-08-14 17:06:02 +0200

    INIT

---
+++ public_html/directives/kylcentralen-navbar.js
@@ -0,0 +1,40 @@
+app.directive("kylcentralenNavbar", ['$log', '$location', '$window', function($log, $location, $window) {
+    return {
+        restrict: 'E',
+        scope: {
+            
+        }, 
+        templateUrl: "./templates/navbar.html",
+        link: function($scope, $attr, $elem) {                            
+            $scope.isActive = function (viewLocation) { 
+                return viewLocation === $location.path();
+            };
+            
+            $scope._toggleNavigation = function() {
+                $scope.navigationOpen = !$scope.navigationOpen;
+            };
+            
+            $scope._navigate = function() {
+                if($window.innerWidth <= 768) {
+                    $scope.navigationOpen = false;
+                }
+            };
+            
+            $scope.validateNavigationState = function() {
+                if($window.innerWidth >= 768) {
+                    $scope.navigationOpen = true;
+                } else {
+                    $scope.navigationOpen = false;
+                }
+            };
+            
+            angular.element($window).on('resize', function(){
+                $scope.$apply(function() {
+                    $scope.validateNavigationState();
+                });                
+            });
+            
+            $scope.validateNavigationState();
+        }
+    };
+}]);
